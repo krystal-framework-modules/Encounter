6280897784e9ec4af4fb57da943876c86aa7d612
dave-yang <daworld.ny@gmail.com>
2019-10-18 03:14:35 +0500

    Added like() and dislike() methods

--- Storage/MySQL/EncounterMapper.php
+++ Storage/MySQL/EncounterMapper.php
@@ -23,4 +23,72 @@ final class EncounterMapper extends AbstractMapper
     {
         return 'id';
     }
+
+    /**
+     * Checks whether reaction has already beet set
+     * 
+     * @param int $senderId Owner id
+     * @param int $receiverId User id to be checked against
+     * @return boolean
+     */
+    private function hasReaction($senderId, $receiverId)
+    {
+        $db = $this->db->select()
+                       ->count('id')
+                       ->from(self::getTableName())
+                       ->whereEquals('sender_id', $senderId)
+                       ->andWhereEquals('receiver_id', $receiverId);
+
+        return (bool) $db->queryScalar();
+    }
+
+    /**
+     * Inserts a reaction
+     * 
+     * @param int $senderId Owner id
+     * @param int $receiverId User id to be liked
+     * @param int $like Whether liked or not
+     * @return boolean
+     */
+    private function insertReaction($senderId, $receiverId, $like)
+    {
+        // Stop, if previously declared
+        if ($this->hasReaction($senderId, $receiverId)) {
+            return false;
+        }
+
+        $data = array(
+            'sender_id' => $senderId,
+            'receiver_id' => $receiverId,
+            'like' => $like
+        );
+
+        $db = $this->db->insert(self::getTableName(), $data);
+
+        return $db->execute();
+    }
+
+    /**
+     * Likes a user
+     * 
+     * @param int $senderId Owner id
+     * @param int $receiverId User id to be liked
+     * @return mixed
+     */
+    public function like($senderId, $receiverId)
+    {
+        return $this->insertReaction($senderId, $receiverId, 1);
+    }
+
+    /**
+     * Dislikes a user
+     * 
+     * @param int $senderId Owner id
+     * @param int $receiverId User id to be disliked
+     * @return mixed
+     */
+    public function dislike($senderId, $receiverId)
+    {
+        return $this->insertReaction($senderId, $receiverId, 0);
+    }
 }
